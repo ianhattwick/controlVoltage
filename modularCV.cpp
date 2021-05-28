52903d190b819f535e4413cc68810b386c48bee7
ianhattwick <ian@ianhattwick.com>
2021-05-28 14:14:13 -0400

    first init

---
+++ modularCV.cpp
@@ -0,0 +1,113 @@
+/*
+
+
+*/
+
+
+/* _____PROJECT INCLUDES_____________________________________________________ */
+#include "modularCV.h"
+
+
+/* _____PUBLIC FUNCTIONS_____________________________________________________ */
+/**
+Constructor.
+
+Creates class object. Initialize buffers
+*/
+modularCV::modularCV()
+{
+
+}
+
+void modularCV::setup() {
+  
+}
+
+
+void modularCV::loop() {
+  if(millis()-_timer > _loopInterval){
+  switch(_state){
+    case 0: //no activity
+    break;
+
+    case 1: //in trigger delay
+    if(millis() - _triggerTimer > _trigLen){
+      _outVal = 0;
+      _state = 0;
+    }
+    break;
+
+    case 2: //in gate delay
+    if(millis() - _triggerTimer > _gateLen){
+      _outVal = 0;
+      _state = 0;
+    }
+    break;
+
+    case 3: //smooth CV
+      _progress = millis() - _lineBegin;
+      if(_progress > _lineLength){
+        _outVal = _goal;
+        _state = 0;
+        break;
+      } else{
+        _outVal = (_goal - _prev) * (_progress/_lineLength) + _prev;
+      }
+      break;
+    }//switch
+  }//timer
+}
+
+void modularCV::trigger() {
+  _outVal = 4095;
+  _triggerTimer = millis();
+  _state = 1;
+}
+
+void modularCV::gate(int val) {
+  if( val > 0 ) _outVal = 4095;
+  else _outVal = 0;
+  _state = 0;
+}
+
+void modularCV::gate(int val, int gateLen) {
+  if( val > 0 ) _outVal = 4095;
+  else _outVal = 0;
+  _gateLen = gateLen;
+  _state = 2;
+}
+
+void modularCV::cv(int val){
+  _prev = _outVal;
+  _goal = val;
+  _state = 3;
+}
+
+void modularCV::cv(int val, int lineTime){
+  _prev = _outVal;
+  _goal = val;
+  _state = 3;
+  _lineLength = lineTime;
+}
+
+void modularCV::midi(byte _val){
+  byte val = _val<24 ? 24 : _val > 72 ? 72 : _val;
+
+  _outVal = (val-24) * (4000/48);
+  _state = 0;
+}
+
+int modularCV::get(){
+  return _outVal;
+}
+
+byte modularCV::available(){
+  return _available;
+}
+
+
+/* _____PRIVATE FUNCTIONS_____________________________________________________ */
+
+/*
+Get current values (input register and eeprom) of modularCV
+*/
---
+++ modularCV.h
@@ -0,0 +1,45 @@
+#ifndef modularCV_h
+#define modularCV_h
+
+#include "Arduino.h"
+
+
+class modularCV
+{
+  public:
+    modularCV();
+
+    void setup();
+    void loop();
+    void trigger();
+    void gate(int val);
+    void gate(int val, int gateLen);
+    void cv(int val);
+    void cv(int val, int lineTime);
+    void midi( byte _val );
+    
+    int get();
+    byte available();
+    int peakVolt = 4095;
+    
+
+  private:
+    uint32_t _timer = 0;
+    byte _loopInterval = 0;
+    byte _available = 0;
+    
+    uint32_t _triggerTimer = 0;
+    byte _trigLen = 3;
+    int _gateLen = 100;
+    byte _state = 0;
+    
+    int _prev = 0;
+    int _goal = 0;
+    uint32_t _lineBegin = 0;
+    uint32_t _progress = 0;
+    int _lineLength = 100;
+    int _outVal = 0;
+  
+    
+};
+#endif
